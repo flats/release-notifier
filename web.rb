e8636bb0f872fa2e97b5a90d10231dd4b50cf121
Dave Flaherty <dave@robador.com>
2016-11-06 22:19:52 -0500

    Basic Semaphore CI webhook endpoint

--- web.rb
+++ web.rb
@@ -5,3 +5,10 @@ require 'dotenv'
 get '/' do
   'Hello world!'
 end
+
+post '/ci-post-deploy' do
+  webhook_json = JSON.parse(request.env["rack.input"].read)
+  pr_number = webhook_json['commit']['message'].match(/request #(\d+) /)[1]
+  logger.info "ci post-deploy webhook received for maji PR #{pr_number}"
+  commit_url = webhook_json['commit']['url']
+end
